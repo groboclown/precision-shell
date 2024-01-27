5f735a3a691a12f1cc57ba94b305418116ee675d
Groboclown <groboclown@gmail.com>
2024-01-26 23:14:52 -0600

    Another attempt at ipv6 builds

--- recipes/_build_all.sh
+++ recipes/_build_all.sh
@@ -14,14 +14,15 @@ CONTAINER_RUNNER="${CONTAINER_RUNNER:-docker}"
 IPV6_PRIVATE_SUBNET="${IPV6_PRIVATE_SUBNET:-fd76:0bca:85f3:17a0::/64}"
 
 # ipv6 is required for some of the networking tests.
-"${CONTAINER_RUNNER}" network ls | grep presh-net >/dev/null 2>&1
-if [ $? != 0 ] ; then
-    
-    "${CONTAINER_RUNNER}" network create --ipv6 --subnet "${IPV6_PRIVATE_SUBNET}" presh-net
-    if [ $? != 0 ] ; then
+if ! "${CONTAINER_RUNNER}" network ls | grep presh-net >/dev/null 2>&1 ; then
+    if \
+        ! "${CONTAINER_RUNNER}" network create --ipv6 --subnet "${IPV6_PRIVATE_SUBNET}" presh-net \
+    ; then
         echo "Failed to create ipv6 network for the tests.  Cannot proceed."
         exit 1
     fi
+    if \
+        ! "${CONTAINER_RUNNER}" buildx create --name=presh-build --driver=docker-container --driver-opt="network=presh-net"
 fi
 
 cd "$( dirname "$0" )/.."
@@ -31,7 +32,7 @@ errs=0
 declare -a err_names
 for name in ${this_dir}/*.Dockerfile ; do
     base="$( basename "${name}" .Dockerfile )"
-    "${CONTAINER_RUNNER}" build --network presh-net -t "local/presh-${base}:build" -f "${name}" .
+    "${CONTAINER_RUNNER}" buildx build --builder=presh-build -t "local/presh-${base}:build" -f "${name}" .
     if [ $? != 0 ] ; then
         errs=$(( errs + 1 ))
         err_names+=("${name}")
